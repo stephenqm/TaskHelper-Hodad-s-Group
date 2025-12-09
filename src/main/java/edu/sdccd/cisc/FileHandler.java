3b22626e9566b8271512b4e66442c2f546c669f6
stephenqm <smckinney006@student.sdccd.edu>
2025-12-08 22:31:33 -0800

    fixed obj variable to event, fixed filename to match param name,

--- src/main/java/edu/sdccd/cisc/FileHandler.java
+++ src/main/java/edu/sdccd/cisc/FileHandler.java
@@ -1,16 +1,17 @@
 package edu.sdccd.cisc;
+
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.io.IOException;
+
 public class FileHandler {
-    public static void writeObjectToFile(Event Event, String fileName) {
-        try (FileOutputStream fos = new FileOutputStream(filename);
+    public static void writeObjectToFile(Event event, String fileName) {  // Changed Event to event
+        try (FileOutputStream fos = new FileOutputStream(fileName);  // Changed filename to fileName
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            oos.writeObject(obj);
-            System.out.println("Object successfully saved to file: " + filename);
+            oos.writeObject(event);  // Changed obj to event
+            System.out.println("Object successfully saved to file: " + fileName);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
-
